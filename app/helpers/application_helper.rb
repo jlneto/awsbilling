c728b654786c6a701d004a564abc1423ac4139e9
José Lopes Neto <jlneto@mcfox.com.br>
2014-06-23 21:22:18 -0300

    Created Mailer and added panels to home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_month
-    Date.today.strftime('%B %y')
+    Date.today.strftime('%B %Y')
   end
 
   def format_date(date)
@@ -25,6 +25,18 @@ module ApplicationHelper
 
   def format_email(email)
     email.split('@')[0]
+  end
+
+  def format_variation(value)
+    perc_class = ''
+    if value
+      if value >= 0
+        perc_class = 'perc-increase glyphicon glyphicon-arrow-up'
+      else
+        perc_class = 'perc-decrease glyphicon glyphicon-arrow-down'
+      end
+      content_tag('span', "#{format_number(value.abs)}%", class: perc_class)
+    end
   end
 
 end
